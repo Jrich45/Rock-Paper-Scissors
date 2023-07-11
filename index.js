cd02784623016303321b023aa788494f23dd156a
Jrich45 <127894540+Jrich45@users.noreply.github.com>
2023-07-11 11:44:43 -0400

    Moved computer & human variables

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-let choices = ["Rock", "Paper", "Scissors"];
-let playerSelection = prompt("Do you choose Rock, Paper or Scissors?")
-let computerSelection = getComputerChoice();
-let winner = playRound(playerSelection, computerSelection);
-let games = 0;
 let human = 0;
 let computer = 0;
+
+let choices = ["Rock", "Paper", "Scissors"];
+let playerSelection = prompt("Do you choose Rock, Paper or Scissors?")
+
+let computerSelection = getComputerChoice();
+let winner = playRound(playerSelection, computerSelection);
+
+
 
 function getComputerChoice() {
     
@@ -22,9 +25,11 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === "Rock") {
         if (computerSelection === "Paper") {
             console.log("You lose!");
+            computer++;
             return ("You lose!");
         } else if (computerSelection === "Scissors") {
             console.log("You win!");
+            human++;
             return ("You win!");
         } else {
             console.log("It's a tie!")
@@ -35,9 +40,11 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === "Paper") {
         if(computerSelection === "Rock") {
             console.log("You win!");
+            human++;
             return ("You win");
         } else if (computerSelection === "Scissors") {
             console.log("You lose!");
+            computer++;
             return ("You lose!");
         } else {
             console.log("It's a tie!")
@@ -48,9 +55,11 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === "Scissors") {
         if (computerSelection === "Rock") {
             console.log("You lose!");
+            computer++;
             return ("You lose!");
         } else if ( computerSelection === "Paper") {
             console.log("You win!");
+            human++;
             return ("You win!");
         } else {
             console.log("It's a tie!")
@@ -63,14 +72,13 @@ function playRound(playerSelection, computerSelection) {
 
 
 
-    function game(playRound) {
+    function game(human, computer) {
+        let game = 0;
         for (let i = 0; i < 5; i++) {
-            if (playRound === "You lose") {
-                computer++;
-                return;
-            } else if (playRound === "You win") {
-                human++;
-                return;
+            if (winner === "You lose") {
+                return computer++;
+            } else if (winner === "You win") {
+                return human++;
             } else {
                 return;
             } 
